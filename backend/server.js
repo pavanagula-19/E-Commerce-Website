9223e42930b7262cda3ed1fee56bc27ce1334e4c
pavanagula-19 <Pavanagulla19@gmail.com>
2023-06-05 22:33:12 +0530

    Server updated

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,12 @@ connectDatabase();
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
   });
+
+  //Unhandle Promise Rejection 
+  process.on("unhandledRejection", err=>{
+    console.log(`Error, ${err.message}`);
+    console.log("shutting down server due to unhandled promise rejection");
+    server.close(()=>{
+      process.exit(1);
+    })
+  })
